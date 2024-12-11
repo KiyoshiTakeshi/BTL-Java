1fee4835da7453ae30bdeaac2fbeba420631ae70
KiyoshiTakeshi <ngoquocviet2003@gmail.com>
2024-12-11 14:44:51 +0700

    update 11/12
    Fix lỗi liên quan đến server

--- storage_app/src/main/java/com/example/storageApp/config/WebConfig.java
+++ storage_app/src/main/java/com/example/storageApp/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         // Cấu hình CORS cho các endpoint bắt đầu với "/api/"
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost") // Cho phép frontend từ http://localhost
+                .allowedOrigins("http://127.0.0.1:8082/", "http://localhost:8081") // URL của frontend
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // Các phương thức HTTP được phép
                 .allowedHeaders("*"); // Cho phép tất cả các header
     }
