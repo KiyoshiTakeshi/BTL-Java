b28a9e6a3c4c1b3219b3b6f0ef2f3cbfc6c3219c
KiyoshiTakeshi <ngoquocviet2003@gmail.com>
2024-12-13 11:00:46 +0700

    update product
    hoàn thiện chức năng sửa, xoá bảng product

--- storage_app/src/main/java/com/example/storageApp/controller/SupplierController.java
+++ storage_app/src/main/java/com/example/storageApp/controller/SupplierController.java
@@ -3,9 +3,8 @@ package com.example.storageApp.controller;
 import com.example.storageApp.dto.SupplierDTO;
 import com.example.storageApp.service.SupplierService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,6 +17,24 @@ public class SupplierController {
     @GetMapping
     public List<SupplierDTO> getSuppliers() {
         return supplierService.getValidSuppliers();
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createSupplier(@RequestBody SupplierDTO supplierDTO) {
+        supplierService.addSupplier(supplierDTO);
+        return ResponseEntity.ok("Supplier created successfully");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateSupplier(@PathVariable Integer id, @RequestBody SupplierDTO supplierDTO) {
+        supplierService.updateSupplier(id, supplierDTO);
+        return ResponseEntity.ok("Supplier updated successfully");
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteSupplier(@PathVariable Integer id) {
+        supplierService.softDeleteSupplier(id);
+        return ResponseEntity.ok("Supplier deleted successfully");
     }
 }
 
--- storage_app/src/main/java/com/example/storageApp/repository/SupplierRepository.java
+++ storage_app/src/main/java/com/example/storageApp/repository/SupplierRepository.java
@@ -3,6 +3,7 @@ import com.example.storageApp.dto.SupplierDTO;
 import com.example.storageApp.model.Suppliers;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,5 +13,8 @@ public interface SupplierRepository extends JpaRepository<Suppliers, Integer> {
     @Query("SELECT new com.example.storageApp.dto.SupplierDTO(s.sid, s.sname, s.semail, s.sphone, s.saddress, s.stype) " +
             "FROM Suppliers s WHERE s.sstatus = 1")
     List<SupplierDTO> findSupplierData();
+
+    @Query("UPDATE Suppliers s SET s.sstatus = 0 WHERE s.sid = :id")
+    void softDeleteById(@Param("id") Integer id);
 }
 
